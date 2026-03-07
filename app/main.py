4f5bc1f7749865c5ec553d5f80e2972a5535de5b
ErsjanKeri <ersjankeri@gmail.com>
2026-03-07 14:09:07 +0100

    fixed bug with validation

--- app/main.py
+++ app/main.py
@@ -85,8 +85,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
     return JSONResponse(
         status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         content={
-            "detail": "Validation error",
-            "errors": errors
+            "detail": errors
         }
     )
 
